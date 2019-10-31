eeb5d9a9d9793799ab133011e9b8548899528998
Jurrian Lammerts <jurrian@lammerts.net>
2019-10-31 19:36:55 +0100

    express grqphql setup

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+          events: [String!]!
+        }
+
+        type RootMutation {
+          createEvent(name: String): String
+        }
+
+        schema {
+          query: RootQuery
+          mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ['bla', 'blabla', 'blablabla'];
+      },
+      createEvent: args => {
+        const { name } = args;
+        return name;
+      }
+    },
+    graphiql: true
+  })
+);
+
+app.listen(3000);
